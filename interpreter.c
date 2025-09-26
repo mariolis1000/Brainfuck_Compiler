7bd33d38a7df4e497ca214e070510e7608e47aac
Marios <mariolis1000@gmail.com>
2025-09-26 11:24:04 +0300

    making it a bit clearer to what happens when i run the intepreter

--- interpreter.c
+++ interpreter.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stlib.h>
 
 const int N = 30000;
 
@@ -56,18 +57,44 @@ void run(unsigned char *tape, int *i){
 }
 
 
-int main(){
+int main(int argc, char *argv[]){
+
+  
+
   unsigned char tape[30000] = {0};
   int i =0;
-  char mode;
-  scanf("%c", &mode); //I should automate that with a bash script/alias
-                      //That 
-  
-  switch(mode){
-    case'r': //calls run , which is my interpreter
-      program(&tape[0], &i);
-      break;
-    case'c': //runs the compiler, which i hope to write eventually
-      break;
+  //char mode;
+  //char program[];
+
+
+  if(argc < 1) {
+    run(&tape[0], &i); //if nothing is inserted, interpretet interactively
   }
+  else{
+    FILE *f = fopen(argv[1]);
+    if(!f){
+      printf("Couldnt open the file\n"); //check if the file isnt opened correctly
+      return 1;
+    }
+    
+
+    if(argc == 1) char mode[] = "run";
+    else if(argc >= 2) char mode[] = argv[2];
+    switch(mode){
+      case "run": //calls run , which is my interpreter
+        run(&tape[0], &i);
+        break;
+      case "compile": //runs the compiler, which i hope to write eventually
+        break;
+      default:
+        printf("I dont know what you want to do\n");
+    }
+
+  }
+
+  //scanf("%c", &mode); //I should automate that with a bash script/alias
+                      //That 
+
+
+  return 0;
 }
