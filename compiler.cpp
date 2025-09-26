69aa3262453d2730d28f4b8e7a28b125f5e62246
Marios <mariolis1000@gmail.com>
2025-09-26 14:00:44 +0300

    wrote a compiler

---
+++ compiler.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    string input;
+    input.reserve(100000);
+    char c;
+
+    while(cin.get(c)){
+        if(c == '>' || c == '<'
+            || c == '+' || c == '-'
+        || c == '.' || c == ','
+        || c == '[' || c == ']')
+            input += c;
+    }
+
+    ofstream myFile("a.c");
+    myFile << "#include<stdio.h>\n#include<stdlib.h>\n#include<string.h>\nconst int N = 30000;\nint main(){\n unsigned char \n tape[30000]={0};\n int i =0;\n char inp;\n";
+    for(int iter=0; iter<input.size(); iter++){
+        char x = input[iter];
+        switch(x){
+            case '>':
+                myFile << "\n if(i<(N-1)) (i)++;";
+                break;
+            case '<':
+                myFile << "\n if(i>0) (i)--;";
+                break;
+
+            case '+':
+                myFile << "\n tape[i]++;";
+                break;
+            case '-':
+                myFile << "\n tape[i]--;";
+                break;
+
+                //Basic I/O
+            case '.':
+                myFile << "\n putchar(tape[i]);";
+                break;
+            case ',':
+                myFile << "\n inp=getchar();\n tape[i] = inp;";
+                break;
+
+
+            //loop implementation
+            case '[':
+               myFile << "\n for(int j=i; j>0; j--){";
+               break;
+            case ']':
+               myFile << "\n }\n";
+                break;
+        }
+    }
+    myFile << "\n return 0;\n}";
+    myFile.close();
+}
